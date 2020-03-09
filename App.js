78816b884e2233e260a05f2a2a10816c8b1f8fbe
Avijit Acharjee <avijitach@gmail.com>
2020-03-10 00:09:27 +0600

    b

--- App.js
+++ App.js
@@ -1,106 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  Button ,
-  TextInput
-} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-class App extends React.Component {
-  constructor ()
-  {
-    super();
-    this.state = {
-      sampleText: 'Initial Text'
-    };
-    
+export default class App extends React.Component {
+  state={
+    email:"",
+    password:""
   }
-  ch = () => 
-    {
-      this.setState({sampleText: 'Avijit Acharjee'})
-    }
-render () {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style = {{
-        padding : 20
-      }}>
-        <ScrollView>
-          <TextInput style = {{
-            borderColor: 'black',
-            borderWidth : 1,
-            padding : 20
-           }}/>
-          <Text style ={{
-            fontSize : 30
-          }}
-          onPress = {this.ch}>
-            {this.state.sampleText}
-            </Text>
-          <Button title="Change text" />
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-}
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>PUC App</Text>
+        <View style={styles.inputView} >
+          <TextInput  
+            style={styles.inputText}
+            placeholder="Email..." 
+            placeholderTextColor="#50FFFFFF"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput  
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password..." 
+            placeholderTextColor="#50FFFFFF"
+            onChangeText={text => this.setState({password:text})}/>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.signUp}>Signup</Text>
+        </TouchableOpacity>
 
-export default App;
+  
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#E0E0E0",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"black"
+  },
+  signUp: {
+    color: "#FFFFFF"
+  }
+});
